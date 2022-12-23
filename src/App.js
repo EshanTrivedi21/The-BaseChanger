5002e78dd027c85968b860a970df8e82d1c43bd2
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-12-24 01:49:13 +0530

    feat: init v2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Box from "@mui/material/Box";
 
 function App() {
   return (
     <>
+      <Box
+        justify="center"
+        sx={{
+          width: "30vw",
+          height: "30vw",
+          backgroundColor: "white",
+          padding: { lg: 0, md: 2, sm: 0 },
+          m: "auto",
+          my: "50vh",
+          transform: "translateY(-50%)",
+          overflow: "hidden",
+        }}
+      >
+      </Box>
     </>
   );
 }
