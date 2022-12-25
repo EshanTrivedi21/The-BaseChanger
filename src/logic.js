96d71cbb0719417f79745a6fbc5546fb6f210086
Milind Nair <milindnair261003@gmail.com>
2022-12-25 19:18:14 +0530

    added validatipn files and conversion of decimal to bcd

--- src/logic.js
+++ src/logic.js
@@ -1,5 +1,64 @@
+function ValidInput(inputType,str) {
+  let isValid = false; 
+  /////// Validation for binary /////////////////////////////
+  if(inputType === 1){
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "0" || str[i] === "1") {
+      isValid = true;
+    } else {
+      isValid =  false;
+      break;
+    }
+  }
+}
+
+
+  ////////////// Validation for Hexadecimal //////////////////////////
+  if(inputType === 3){
+  const HexaValues = ['0','1','2','3','4','5','6','7','8','9','10','A','B','C','D','E','F'];
+  
+  for(let i = 0; i < str.length; i++) {
+    let currValue = str[i] ;
+    if(HexaValues.includes(currValue)){
+      isValid = true;
+    }
+    else{
+      isValid = false;
+      break;
+    }
+  }
+}
+
+//////////////////// Validation for Octal //////////////////////////
+if(inputType === 2){
+  const OctalValues = ['0','1','2','3','4','5','6','7'];
+  
+  for(let i = 0; i < str.length; i++) {
+    let currValue = str[i] ;
+    if(OctalValues.includes(currValue)){
+      isValid = true;
+    }
+    else{
+      isValid = false;
+      break;
+    }
+  }
+}
+
+  console.log(isValid);
+  return isValid;
+};
+
 const BinarytoDecimal = (inp) => {
-  return parseInt(inp, 2);
+  // const isBinary = ValidInput(inp);
+  if(ValidInput(1,inp) === true){
+    return parseInt(inp, 2);
+  }
+  else{
+    return 0;
+  }
+  
 };
 
 const DecimaltoBinary = (inp) => {
@@ -7,7 +66,12 @@ const DecimaltoBinary = (inp) => {
 };
 
 const OctaltoDecimal = (inp) => {
+  if(ValidInput(2,inp) === true){
   return parseInt(inp, 8);
+  }
+  else{
+    return 0;
+  }
 };
 
 const DecimaltoOctal = (inp) => {
@@ -15,7 +79,12 @@ const DecimaltoOctal = (inp) => {
 };
 
 const HexadecimaltoDecimal = (inp) => {
+  if(ValidInput(3,inp) === true){
   return parseInt(inp, 16);
+  }
+  else{
+    return 0;
+  }
 };
 
 const DecimaltoHexadecimal = (inp) => {
@@ -26,8 +95,41 @@ const BCDtoDecimal = () => {
   //code
 };
 
-const DecimaltoBCD = () => {
-  //code
+const DecimaltoBCD = (n) => {
+  let str = " ";
+  if (n == 0) {
+    return ("0000");
+}
+
+// To store the reverse of n
+let rev = 0;
+
+// Reversing the digits
+while (n > 0) {
+    rev = rev * 10 + (n % 10);
+    n = parseInt(n / 10, 10);
+}
+
+// Iterate through all digits in rev
+while (rev > 0) {
+
+    // Find Binary for each digit
+    // using bitset
+    let b = (rev % 10).toString(2);
+    while(b.length != 4)
+    {
+        b = "0" + b;
+    }
+
+    // Print the Binary conversion
+    // for current digit
+    str = str +(b + " ");
+
+    // Divide rev by 10 for next digit
+    rev = parseInt(rev / 10, 10);
+}
+return str;
+  
 };
 
 const XS3toDecimal = () => {
@@ -72,6 +174,6 @@ export default function logic(inputType, outputType, inputValue) {
   } else if (inputType === 6 && outputType === 0) {
     return GraytoDecimal(inputValue);
   } else {
-    return inputValue;
+    return 0;
   }
 }
