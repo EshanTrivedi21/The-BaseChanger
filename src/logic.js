a7513c5f8ccd191b0cdc029f9ca8c49d0c86d10c
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-12-26 23:33:47 +0530

    refactor: cleaned the code

--- src/logic.js
+++ src/logic.js
@@ -24,86 +24,93 @@ function ValidInput(inputType, str) {
       for (let i = 0; i < str.length; i++) {
         if (str[i] === "0" || str[i] === "1") isValid = true; else { isValid = false; break;} 
       } break;
+    case "XS3":
+      if (str.length % 4 !== 0) break;
+      for (let i = 0; i < str.length; i++) {
+        if (str[i] === "0" || str[i] === "1") isValid = true; else { isValid = false; break;} 
+      } break;
+    case "Gray":
+      for (let i = 0; i < str.length; i++) {
+        if (str[i] === "0" || str[i] === "1") isValid = true; else { isValid = false; break;}
+      } break;
     default: break;
   }
   if (isValid === true) return str;
 }
 
 function InputtoDecimal(inputType, inputValue) {
-  switch (inputType) {
-    case "Decimal":
-      return ValidInput("Decimal", inputValue) ? inputValue : "Invalid Input";
-    case "Binary": 
-      return ValidInput("Binary", inputValue) ? parseInt(inputValue, 2) : "Invalid Input";
-    case "Octal":
-      return ValidInput("Octal", inputValue) ? parseInt(inputValue, 8) : "Invalid Input";
-    case "Hexadecimal":
-      return ValidInput("Hexadecimal", inputValue) ? parseInt(inputValue, 16) : "Invalid Input";
-    case "BCD":
-      let strBCD = "";
-      if (ValidInput("BCD", inputValue)) {
-        let arr = inputValue.match(/.{1,4}/g);
-        for (let i = 0; i < arr.length; i++) {
-          strBCD += parseInt(arr[i], 2).toString();
+  if (ValidInput(inputType, inputValue)) {
+    switch (inputType) {
+      case "Binary": 
+        return parseInt(inputValue, 2);
+      case "Octal":
+        return parseInt(inputValue, 8);
+      case "Hexadecimal":
+        return parseInt(inputValue, 16);
+      case "BCD":
+        let strBCD = "";
+        let arrBCD = inputValue.match(/.{1,4}/g);
+        for (let i = 0; i < arrBCD.length; i++) {
+          strBCD += parseInt(arrBCD[i], 2).toString();
         }
-      }
-      return strBCD ? strBCD : "Invalid Input";
-    case "XS3":
-      let strXS3 = "";
-      if (ValidInput("BCD", inputValue)) {
-        let arr = inputValue.match(/.{1,4}/g);
-        for (let i = 0; i < arr.length; i++) {
-          strXS3 += (parseInt(arr[i], 2) - 3).toString();
+        return strBCD ? strBCD : "Invalid Input";
+      case "XS3":
+        let strXS3 = "";
+        let arrXS3 = inputValue.match(/.{1,4}/g);
+        for (let i = 0; i < arrXS3.length; i++) {
+          strXS3 += (parseInt(arrXS3[i], 2) - 3).toString();
         }
-      }
-      return strXS3 ? strXS3 : "Invalid Input";
-    case "Gray":
-      let strGray = 0, flag = false; 
-      if (ValidInput("Binary", inputValue)) {
+        return strXS3 ? strXS3 : "Invalid Input";
+      case "Gray":
+        let strGray = 0, flag = false; 
         let n = parseInt(inputValue, 2);
         for (; n; n = n >> 1) {
           strGray ^= n; flag = true;
         }
-      }
-      return flag ? strGray : "Invalid Input";
-    default: break;
+        return flag ? strGray : "Invalid Input";
+      default: break;
+    }
+  } else {
+    return "Invalid Input";
   }
 }
 
 function DecimaltoOutput(outputType, inputValue) {
-  switch (outputType) {
-    case "Binary":
-      return ValidInput("Decimal", inputValue) ? Number(parseInt(inputValue)).toString(2) : "Invalid Input";
-    case "Octal":
-      return ValidInput("Decimal", inputValue) ? Number(parseInt(inputValue)).toString(8) : "Invalid Input";
-    case "Hexadecimal":
-      return ValidInput("Decimal", inputValue) ? Number(parseInt(inputValue)).toString(16) : "Invalid Input";
-    case "BCD":
-      let strBCD = "";
-      if (ValidInput("Decimal", inputValue)) {
-        let arr = inputValue.split("");
-        for (let i = 0; i < arr.length; i++) {
-          strBCD += parseInt(arr[i], 10).toString(2).padStart(4, "0");
+  if (ValidInput("Decimal", inputValue)) {
+    switch (outputType) {
+      case "Binary":
+        return Number(parseInt(inputValue)).toString(2);
+      case "Octal":
+        return Number(parseInt(inputValue)).toString(8);
+      case "Hexadecimal":
+        return Number(parseInt(inputValue)).toString(16);
+      case "BCD":
+        let strBCD = "";
+        let arrBCD = inputValue.split("");
+        for (let i = 0; i < arrBCD.length; i++) {
+          strBCD += parseInt(arrBCD[i], 10).toString(2).padStart(4, "0");
         }
-      }
-      return strBCD ? strBCD : "Invalid Input";
-    case "XS3":
-      let strXS3 = "";
-      if (ValidInput("Decimal", inputValue)) {
-        let arr = inputValue.split("");
-        for (let i = 0; i < arr.length; i++) {
-          strXS3 += (parseInt(arr[i], 10) + 3).toString(2).padStart(4, "0");
+        return strBCD ? strBCD : "Invalid Input";
+      case "XS3":
+        let strXS3 = "";
+        let arrXS3 = inputValue.split("");
+        for (let i = 0; i < arrXS3.length; i++) {
+          strXS3 += (parseInt(arrXS3[i], 10) + 3).toString(2).padStart(4, "0");
         }
-      }
-      return strXS3 ? strXS3 : "Invalid Input";
-    case "Gray":
-      return ValidInput("Decimal", inputValue) ? Number(parseInt((inputValue ^ (inputValue >> 1)))).toString(2) : "Invalid Input";
-    default: break;
+        return strXS3 ? strXS3 : "Invalid Input";
+      case "Gray":
+        return Number(parseInt((inputValue ^ (inputValue >> 1)))).toString(2);
+      default: break;
     }
+  } else {
+    return "Invalid Input";
+  }
 }
 
 export default function logic(inputType, outputType, inputValue) {
-  if (inputType === "Decimal" || outputType === "Decimal") {
+  if (inputType === outputType) {
+    return ValidInput(inputType, inputValue) ? inputValue : "Invalid Input";
+  } else if (inputType === "Decimal" || outputType === "Decimal") {
     if (inputType === "Decimal") {
       return DecimaltoOutput(outputType, inputValue);
     } else {
